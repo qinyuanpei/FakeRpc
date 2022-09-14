410978e3fa08a5c721687c10e5ccca5860946c4e
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-09-14 12:09:46 +0800

    添加 ConnectionHandler

---
+++ src/FakeRpc.Server/Middlewares/FakeRpcTcpConnectionHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Connections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeRpc.Server.Middlewares
+{
+    public class FakeRpcTcpConnectionHandler : ConnectionHandler
+    {
+        public override Task OnConnectedAsync(ConnectionContext connection)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
